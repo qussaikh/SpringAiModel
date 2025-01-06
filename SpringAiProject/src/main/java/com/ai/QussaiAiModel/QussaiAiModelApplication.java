4aae5cbb343875502282da60d0252d6cc6ff888a
Qussai Khalil <106125439+qussaikh@users.noreply.github.com>
2025-01-06 02:24:39 +0100

    Spring Ai using openAi

---
+++ SpringAiProject/src/main/java/com/ai/QussaiAiModel/QussaiAiModelApplication.java
@@ -0,0 +1,13 @@
+package com.ai.QussaiAiModel;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class QussaiAiModelApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(QussaiAiModelApplication.class, args);
+	}
+
+}
